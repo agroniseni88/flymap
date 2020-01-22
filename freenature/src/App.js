34723fe405ccea66beff9e4fbff63051c2ac59d5
agron.iseni@hotmail.co.uk <agron.iseni@hotmail.co.uk>
2020-01-22 16:43:45 +0100

    added map

--- freenature/src/App.js
+++ freenature/src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Map as LeafletMap, TileLayer, Marker, Popup } from 'react-leaflet';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Map extends React.Component {
+  render() {
+    return (
+      <div class="body">
+            <div class='map'>
+
+
+            <LeafletMap
+              center={[50, 10]}
+              zoom={4}
+              maxZoom={10}
+              attributionControl={true}
+              zoomControl={true}
+              doubleClickZoom={true}
+              scrollWheelZoom={true}
+              dragging={true}
+              animate={true}
+              easeLinearity={0.35}
+            >
+              <TileLayer 
+                url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
+              />
+              
+              <Marker position={[50, 10]}>
+                <Popup>
+                  Popup for any custom information.
+                </Popup>
+              </Marker>
+            </LeafletMap>
+            </div>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default Map
