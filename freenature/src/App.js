0a3f180f5c27c4751a0674a0fd8fe3275e2be5e0
agron.iseni@hotmail.co.uk <agron.iseni@hotmail.co.uk>
2020-01-23 11:03:46 +0100

    logo changed and boostrap

--- freenature/src/App.js
+++ freenature/src/App.js
@@ -1,39 +1,39 @@
 import React from 'react'
-import { Map as LeafletMap, TileLayer, Marker, Popup } from 'react-leaflet';
+import { Map as LeafletMap, GeoJSON, Marker, Popup } from 'react-leaflet';
+import worldGeoJSON from 'geojson-world-map';
 
-class Map extends React.Component {
+class GeoJsonMap extends React.Component {
   render() {
     return (
-      <div class="body">
-            <div class='map'>
-
-
-            <LeafletMap
-              center={[50, 10]}
-              zoom={4}
-              maxZoom={10}
-              attributionControl={true}
-              zoomControl={true}
-              doubleClickZoom={true}
-              scrollWheelZoom={true}
-              dragging={true}
-              animate={true}
-              easeLinearity={0.35}
-            >
-              <TileLayer 
-                url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
-              />
-              
-              <Marker position={[50, 10]}>
-                <Popup>
-                  Popup for any custom information.
-                </Popup>
-              </Marker>
-            </LeafletMap>
-            </div>
-      </div>
+      <LeafletMap
+        center={[50, 10]}
+        zoom={6}
+        maxZoom={10}
+        attributionControl={true}
+        zoomControl={true}
+        doubleClickZoom={true}
+        scrollWheelZoom={true}
+        dragging={true}
+        animate={true}
+        easeLinearity={0.35}
+      >
+        <GeoJSON
+          data={worldGeoJSON}
+          style={() => ({
+            color: '#4a83ec',
+            weight: 0.5,
+            fillColor: "#1a1d62",
+            fillOpacity: 1,
+          })}
+        />
+        <Marker position={[50, 10]}>
+          <Popup>
+            Popup for any custom information.
+          </Popup>
+        </Marker>
+      </LeafletMap>
     );
   }
 }
 
-export default Map
+export default GeoJsonMap
