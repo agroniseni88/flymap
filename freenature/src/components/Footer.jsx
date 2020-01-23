fc8eba5b48e0ce96058d9bbd66afe9e802782037
Tamar <asashvili@gmail.com>
2020-01-23 10:28:23 +0100

    added components

---
+++ freenature/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import nature from '../img/nature.png';
+
+
+
+export default function Footer() {
+    return (
+        <div>
+            <img src={nature} alt="nature" style={{ width: '100px', height: '100px' }} />;
+        </div>
+    )
+}
