39edac5dc4acbeb949bbcfaf11480c065d33f23d
agron.iseni@hotmail.co.uk <agron.iseni@hotmail.co.uk>
2020-01-27 10:20:34 +0100

    g

--- freenature/src/App.js
+++ freenature/src/App.js
@@ -1,39 +1,27 @@
 import React from 'react'
-import { Map as LeafletMap, GeoJSON, Marker, Popup } from 'react-leaflet';
-import worldGeoJSON from 'geojson-world-map';
+import Maps from './components/map'
+import Ic from './components/icons'
 
-class GeoJsonMap extends React.Component {
-  render() {
-    return (
-      <LeafletMap
-        center={[50, 10]}
-        zoom={6}
-        maxZoom={10}
-        attributionControl={true}
-        zoomControl={true}
-        doubleClickZoom={true}
-        scrollWheelZoom={true}
-        dragging={true}
-        animate={true}
-        easeLinearity={0.35}
-      >
-        <GeoJSON
-          data={worldGeoJSON}
-          style={() => ({
-            color: '#4a83ec',
-            weight: 0.5,
-            fillColor: "#1a1d62",
-            fillOpacity: 1,
-          })}
-        />
-        <Marker position={[50, 10]}>
-          <Popup>
-            Popup for any custom information.
-          </Popup>
-        </Marker>
-      </LeafletMap>
-    );
+
+class App extends React.Component {
+  render(){
+    return(
+     <div>
+
+         <Maps /> 
+         <Ic />
+     
+     
+     </div>
+        
+         
+        
+       
+    
+     
+    )
   }
 }
 
-export default GeoJsonMap
+export default App;
+
