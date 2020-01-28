b0a0fa4f6ce432f84de08a54db3bc532dd2794a0
Tamar <asashvili@gmail.com>
2020-01-28 20:42:06 +0100

    img icon button

--- freenature/src/components/button/ButtonF.jsx
+++ freenature/src/components/button/ButtonF.jsx
@@ -3,16 +3,43 @@ import { Button } from 'react-bootstrap';
 
 
 
+
 export default function ButtonF() {
     return (
-        <div>
-            <div className="mb-2">
-                <Button className='btn' variant="outline-warning" size="lg">
+
+        <div id="flex_button">
+            {/*  <div className="mb-2">
+             <Button className='btn' variant="outline-warning" size="lg">
                     Large button
     </Button>{' '}
                 <Button className='btn' variant="outline-warning" size="lg">
                     Large button
     </Button>
+    
+            </div>*/}
+            <div className="btn_button">
+                <div className="box_button">
+                    <i className="fas fa-bicycle"></i>
+                </div>
+
+            </div>
+            <div className="btn_button">
+                <div className='box_button'>
+                    <i class="fas fa-swimmer"></i>
+                </div>
+
+            </div>
+            <div className="btn_button">
+                <div className="box_button">
+                    <i class="fas fa-air-freshener"></i>
+                </div>
+
+            </div>
+            <div className="btn_button">
+                <div className='box_button'>
+                    <i class="fas fa-dog"></i>
+                </div>
+
             </div>
         </div>
     )
