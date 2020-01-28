63a59c27ba3d8dc1ce5b17513da7bacbf91fd06e
sena <avci.msena@gmail.com>
2020-01-28 17:19:24 +0100

    adding polygons and belgium coordinates to the map

--- freenature/src/components/MapBe.jsx
+++ freenature/src/components/MapBe.jsx
@@ -1,16 +1,22 @@
 import React from 'react';
-import { Map as LeafletMap, Polygon, GeoJSON, TileLayer, Popup } from 'react-leaflet';
+import { Map as LeafletMap, GeoJSON, TileLayer, Popup } from 'react-leaflet';
 import PopUp from './PopUp';
 import bivakzones from './bivakzones.json';
 import Icon from './Symbols'
 
 class MapBe extends React.Component {
+  state = {
+    lat: 50.502,
+    lng: 4.34878,
+  }
+
     render() {
+      const position =  [this.state.lat, this.state.lng]
         return (
     <div>
             <LeafletMap
-                center={[51, 5]}
-                zoom={9}
+                center={position}
+                zoom={8}
                 maxZoom={19}
                 attributionControl={true}
                 zoomControl={true}
@@ -20,10 +26,25 @@ class MapBe extends React.Component {
                 animate={true}
                 easeLinearity={0.35}
             >
-                <TileLayer url='https://tiles.wmflabs.org/hikebike/{z}/{x}/{y}.png' attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors' />
-              
+                <TileLayer url="https://api.maptiler.com/maps/streets/{z}/{x}/{y}.png?key=CrxeqLRPyjGPIdZII2Ej"
+          attribution="&copy; <a href=&quot;https://www.maptiler.com/copyright/&quot;>OpenStreetMap</a>"
+        />
                 {
-                   bivakzones.features.map((bivakzone) => (
+                   bivakzones.features
+                      .map((bivakzone)=> {
+                        if(bivakzone.geometry.type === 'Point') {
+                          return(bivakzone)
+                        }
+                        else {
+                          bivakzone.geometry.coordinates=
+                           bivakzone.geometry.coordinates[0][0]
+                           bivakzone.geometry.type = 'Point'
+                           return(bivakzone)
+                        }
+                      })
+                   
+                   
+                      .map((bivakzone) => (
                         <GeoJSON
                             data={bivakzone}
                             style={() => ({
