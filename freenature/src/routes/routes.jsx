bf1ffad43e2ca14205082368cad110cc5e67741a
sena <avci.msena@gmail.com>
2020-01-28 14:20:35 +0100

    drinking water is added

--- freenature/src/routes/routes.jsx
+++ freenature/src/routes/routes.jsx
@@ -4,6 +4,7 @@ import NotFound from '../components/NotFound';
 import Home from '../components/Home';
 import Marker from '../components/Marker';
 import Bike from '../components/Bicyclee';
+import DrinkingWater from '../components/DrinkingWater'
 
 
 
@@ -16,6 +17,7 @@ class MyRoutes extends React.Component {
                 <Route exact path="/" component={Home} />
                 <Route exact path="/marker" component={Marker} />
                 <Route exact path="/bicycle" component={Bike} />
+                <Route exact path="/drinkingwater" component={DrinkingWater} />
                 <Route exact path="/notfound" component={NotFound} />
                 <Route path="*" component={NotFound} />
             </Switch>
