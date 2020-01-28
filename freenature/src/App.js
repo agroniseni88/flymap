bdcd8739f3bbb6afc2a99f880478738a350daac9
Pieter Vander Vennet <pietervdvn@gmail.com>
2020-01-28 15:35:07 +0100

    Fixed merge conflicts

--- freenature/src/App.js
+++ freenature/src/App.js
@@ -1,15 +1,4 @@
-<<<<<<< HEAD
-import React from 'react'
-import Question from './icons';
-class Myapp extends React.Component {
-  render() {
-    return (
-    <div>
-      <Question  />
-    </div>  
-    );
-  }
-=======
+
 import React from 'react';
 import MapBe from './components/MapBe';
 import Header from './components/Header';
@@ -30,7 +19,6 @@ function App() {
       <Footer />
     </div>
   );
->>>>>>> 7bbfead6a2c34913eab4a62c1ad65d7f596b7712
 }
 
-export default Myapp
+export default App
