66fbdde476d264317d805d1932b0c88841a45238
Tamar <asashvili@gmail.com>
2020-02-03 14:35:57 +0100

    footer

--- freenature/src/components/Icon.jsx
+++ freenature/src/components/Icon.jsx
@@ -1,86 +1,89 @@
-import React, {Component} from 'react';
-import bivakzones from '../components/bivakzones.json';
+import React, { Component } from 'react';
 
-class Icon extends Component{
-    constructor(props){
+
+class Icon extends Component {
+    constructor(props) {
         super(props)
-        this.state ={
-                  preference: false,
-                  color: false
-                  
-        } 
-        this.handleClick = this.handleClick.bind(this);
-    }   
+        this.state = {
+            preference: false,
+            color: false
 
-handleClick(e){
-    
-    var txt = "Dog: no preferences";
-    if(this.state.preference === true) {
-        txt = "Dogs: allowed";
-         
-    }else if(this.state.preference === false) {
-        txt = "Dogs: not allowed"; 
-        // console.log(bivakzones)
-         
-    
-    }else{
-        this.setState.preference = 0;   
-    
-        
+        }
+        this.handleClick = this.handleClick.bind(this);
     }
 
-    this.setState({
-        preference : this.state.preference + 1,
-        color: !this.state.color,
-        text: txt
-      })
-      
-          
-}
+    handleClick(e) {
 
-   render(){
-    const isGreen = this.state.color;
-    return (
-        <div >
-            
-            
-            
+        var txt = "Dog: no preferences";
+        if (this.state.preference === true) {
+            txt = "Dogs: allowed";
 
-            <button  
-             style={{backgroundColor: isGreen?  'green':'red'}}
-             onClick={this.handleClick} 
-             className={this.state.button ? "buttonTrue": "buttonFalse"} >
-                <i className="fas fa-dog fa-2x"> </i>
-            </button>
-        
-
-            <button 
-            style={{
-            margin: '18px',}}>
-                <i className="fas fa-toilet fa-2x"> </i>
-            </button>
-            
-            <button 
-            style={{
-            margin: '18px',}}>
-                <i className="fas fa-fire fa-2x"> </i>
-            </button>
-
-            <button 
-            style={{
-            margin: '18px',}} >              <i className="fas fa-bicycle fa-2x"> </i>
-            </button>
-
-            
+        } else if (this.state.preference === false) {
+            txt = "Dogs: not allowed";
 
 
 
-    <h1 style={{fontSize: '14px', backgroundColor: isGreen?  'green':'red'}}> {this.state.text}</h1>
-    
+        } else {
+            this.setState.preference = 0;
 
-        </div>
-       )
-   }
+
+        }
+
+        this.setState({
+            preference: this.state.preference + 1,
+            color: !this.state.color,
+            text: txt
+        })
+
+
+    }
+
+    render() {
+        const isGreen = this.state.color;
+        return (
+            <div >
+
+
+
+
+                <button
+                    style={{ backgroundColor: isGreen ? 'green' : 'red' }}
+                    onClick={this.handleClick}
+                    className={this.state.button ? "buttonTrue" : "buttonFalse"} >
+                    <i className="fas fa-dog fa-2x"> </i>
+                </button>
+
+
+                <button
+                    style={{
+                        margin: '18px',
+                    }}>
+                    <i className="fas fa-toilet fa-2x"> </i>
+                </button>
+
+                <button
+                    style={{
+                        margin: '18px',
+                    }}>
+                    <i className="fas fa-fire fa-2x"> </i>
+                </button>
+
+                <button
+                    style={{
+                        margin: '18px',
+                    }} >              <i className="fas fa-bicycle fa-2x"> </i>
+                </button>
+
+
+
+
+
+                <h1 style={{ fontSize: '14px', backgroundColor: isGreen ? 'green' : 'red' }}> {this.state.text}</h1>
+
+
+            </div>
+        )
+    }
 }
 
 export default Icon;
