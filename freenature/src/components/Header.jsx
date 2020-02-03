f4a8ec4e38aa7c7b248f562c9890fec917e46299
sena <avci.msena@gmail.com>
2020-02-03 11:46:41 +0100

    changed the classes to className

--- freenature/src/components/Header.jsx
+++ freenature/src/components/Header.jsx
@@ -15,11 +15,11 @@ export default function Header() {
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="mr-auto">
-                            <Nav.Link href="/"> <Button variant="outline-success"> <i class="fas fa-globe-europe"> <i className="fas fa-map-marked-alt"></i></i></Button></Nav.Link>
+                            <Nav.Link href="/"> <Button variant="outline-success"> <i className="fas fa-globe-europe"> <i className="fas fa-map-marked-alt"></i></i></Button></Nav.Link>
                             <Nav.Link href="/water"> <Button variant="warning">   <i className="fas fa-dog "> <i className="fas fa-map-marked-alt"></i></i></Button></Nav.Link>
                             <Nav.Link href="/404"> <Button variant="warning">   <i className="fas fa-star "> <i className="fas fa-map-marked-alt"></i></i></Button></Nav.Link>
                             <Nav.Link href="/404"> <Button variant="warning">   <i className="fas fa-star "> <i className="fas fa-map-marked-alt"></i></i></Button></Nav.Link>
-                            <Nav.Link href="/404"> <Button variant="warning">   <i class="fas fa-tint-slash"><i className="fas fa-map-marked-alt"></i></i></Button></Nav.Link>
+                            <Nav.Link href="/404"> <Button variant="warning">   <i className="fas fa-tint-slash"><i className="fas fa-map-marked-alt"></i></i></Button></Nav.Link>
                         </Nav>
                     </Navbar.Collapse>
                 </Navbar>
