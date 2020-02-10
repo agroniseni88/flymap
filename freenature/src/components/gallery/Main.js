fb9a1f000c70cdd294952bf74c1f36a490074d67
Tamar <asashvili@gmail.com>
2020-02-10 14:25:43 +0100

    test1

--- freenature/src/components/gallery/Main.js
+++ freenature/src/components/gallery/Main.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import Title from './Title';
 import { Route, Link } from 'react-router-dom';
 import bivakzones from '../bivakzones.json';
-import { Card, Container, Col } from 'react-bootstrap';
+import { Card, Container, Col, ListGroup, ListGroupItem } from 'react-bootstrap';
 
 class Main extends Component {
 
@@ -38,7 +38,13 @@ class Main extends Component {
 
                                                     <Link to='/'><Card.Img className='photo' bg="info" variant="top" src={`${bivakzone.properties.image}`} thumbnail /></Link>
                                                     <Card.Title>{bivakzone.properties.name}</Card.Title>
-                                                    <a href={bivakzone.properties.website}><i class="fas fa-map-marked-alt"></i></a>
+                                                    <ListGroup variant="flush">
+                                                        <ListGroupItem>Cras justo odio</ListGroupItem>
+                                                        <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
+                                                        <ListGroupItem>Vestibulum at eros</ListGroupItem>
+                                                    </ListGroup>
+                                                    <Card.Link href={bivakzone.properties.website} style={{ color: 'green' }}><i class="fas fa-seedling"></i>see Website</Card.Link>
+
                                                 </Card>
                                             </Col>
                                         </Container>
